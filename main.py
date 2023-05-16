31d900b2e216e5dbfddb67a5647ff40f28d0272b
Julio Jose Mielniczuk Lapp <82829895+juliom86@users.noreply.github.com>
2023-05-16 10:32:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def retorno(pelicula: str):
 @app.get('/recomendacion/{titulo}')
 def recomendacion(titulo: str):
     '''Ingresasun nombre de pelicula y te recomienda las similares en una lista'''
-    i = pd.read_csv("titulos.csv").iloc[:7000]
+    i = pd.read_csv("titulos.csv").iloc[:5000]
     tfidf = TfidfVectorizer(stop_words="english")
     i["overview"] = i["overview"].fillna("")
 
